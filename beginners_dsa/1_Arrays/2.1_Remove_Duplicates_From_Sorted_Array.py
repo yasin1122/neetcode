b3af025105dd9bd64d05e16bcd92d913f2b08fec
yasin1122 <whoisyasin@gmail.com>
2024-07-03 00:41:52 +0300

    remove element complete

--- beginners_dsa/1_Arrays/2.1_Remove_Duplicates_From_Sorted_Array.py
+++ beginners_dsa/1_Arrays/2.1_Remove_Duplicates_From_Sorted_Array.py
@@ -54,7 +54,6 @@ class Solution:
         #     nums.append("_")
         
         # return new_len
-        
         j = 1
         for i in range(1, len(nums)):
             if nums[i] != nums[i-1]:
