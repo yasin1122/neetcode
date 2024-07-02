75d733035b5fe18119263dc56923c2a4feac819f
yasin1122 <whoisyasin@gmail.com>
2024-07-02 17:56:32 +0300

    beginner_dsa setup

---
+++ beginners_dsa/1_Arrays/1_RAM/ram.py
@@ -0,0 +1,13 @@
+# Random Access Memory
+
+"""
+Byte = 8 bits
+Bits are 0 or 1
+
+An Integer uses 4 Bytes = 32 Bits
+Integer 1 = 0000 0000 ... 0001 (32 bits)
+ASCII Characters only take 1 Byte
+
+Arrays are stored continuously 
+
+"""
