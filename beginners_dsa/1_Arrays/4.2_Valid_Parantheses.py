853cb14240d48d891ce8f9cb026fede28350914a
yasin1122 <whoisyasin@gmail.com>
2024-07-09 18:13:37 +0300

    4.2 done

--- beginners_dsa/1_Arrays/4.2_Valid_Parantheses.py
+++ beginners_dsa/1_Arrays/4.2_Valid_Parantheses.py
@@ -28,3 +28,22 @@ Constraints:
 s consists of parentheses only '()[]{}'.
 """
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
+    
+    
+sol = Solution()
+s = "({[)]}"
+print(sol.isValid(s))
