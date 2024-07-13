95615c9f50b95ab8c232d82030316456217611fd
yasin1122 <whoisyasin@gmail.com>
2024-07-13 22:20:28 +0300

    append

--- dsa_self_implementations/singly_linked_list.py
+++ dsa_self_implementations/singly_linked_list.py
@@ -25,7 +25,17 @@ class LinkedList:
         
     # append O(1)
     def append(self, value) -> bool:
-        pass
+        new_node = Node(value)
+        
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+        self.length += 1
+        return True
 
     # pop O(n) (last item)
     def pop(self) -> Node:
