314ea38b52f442fd6960d1639061ae992d32bace
yasin1122 <whoisyasin@gmail.com>
2024-07-13 21:53:37 +0300

    ll setup

---
+++ dsa_self_implementations/singly_linked_list.py
@@ -0,0 +1,53 @@
+# Node Class
+class Node:
+
+    # Constructor
+    def __init__(self, value) -> None:
+        pass
+
+# Linked List Class
+class LinkedList:
+
+    # Constructor
+    def __init__(self, value) -> None:
+        pass
+
+    # print list
+    def print_list(self) -> None:
+        pass
+        
+    # append
+    def append(self, value) -> bool:
+        pass
+
+    # pop (last item)
+    def pop(self) -> Node:
+        pass
+
+    # prepend
+    def prepend(self, value) -> bool:
+        pass
+
+    # pop (first item)
+    def pop_first(self) -> Node:
+        pass
+
+    # get
+    def get(self, index) -> Node:
+        pass
+
+    # set value (change value at a set index)
+    def set_value(self, index, value) -> bool:
+        pass
+
+    # insert (new value at an index)
+    def insert(self, index, value) -> bool:
+        pass
+
+    # remove (value at a given index)
+    def remove(self, index) -> Node:
+        pass
+
+    # reverse (the linked list)
+    def reverse(self) -> None:
+        pass
