6a7bd3edf3208018801f5afef2d5883d39033e0b
yasin1122 <whoisyasin@gmail.com>
2024-07-13 22:30:00 +0300

    prepend

--- dsa_self_implementations/singly_linked_list.py
+++ dsa_self_implementations/singly_linked_list.py
@@ -60,7 +60,17 @@ class LinkedList:
 
     # prepend O(1)
     def prepend(self, value) -> bool:
-        pass
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+        return True
+
 
     # pop O(1) (first item)
     def pop_first(self) -> Node:
